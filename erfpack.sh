684d8b05587fa92ffa864cecad565b81aaaf605b
Henning Koehler <henning.koehler.nz@gmail.com>
2018-06-19 19:22:26 +1200

    added scripts for conversion to erf

---
+++ erfpack.sh
@@ -0,0 +1,3 @@
+ERFPACK="perl bin/erfpack.pl -e -o"
+export PERL5LIB=perllib
+$ERFPACK hk_inc.erf hk_inc_*.nss
---
+++ setup.sh
@@ -0,0 +1,7 @@
+# download & extract erfpack
+wget http://www.kivinen.iki.fi/nwn/modules-1.23.tar.gz
+wget http://www.kivinen.iki.fi/nwn/genericbins-1.23.tar.gz
+tar xf modules-1.23.tar.gz
+tar xf genericbins-1.23.tar.gz
+rm modules-1.23.tar.gz
+rm genericbins-1.23.tar.gz
