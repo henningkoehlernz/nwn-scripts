bb3652c79db7580b010c8fb7e32b6acb13afd023
Henning Koehler <henning.koehler.nz@gmail.com>
2019-05-05 12:27:21 +1200

    added format2da.py

---
+++ format2da.py
@@ -0,0 +1,19 @@
+# format 2DA file for readability
+import sys
+
+# read input
+raw2da = [ line.split() for line in sys.stdin ]
+# push 3rd line entry (header) back one column
+raw2da[2].insert(0, "")
+# find max number of columns
+column_count = max( len(row) for row in raw2da )
+# find max width of each column
+column_max_width = [ max( len(row[col] ) for row in raw2da if len(row) > col ) for col in range(column_count) ]
+# set target width to smallest value = 1 mod tab-width
+TAB_WIDTH = 4
+column_width = [ width + (TAB_WIDTH - 1 - width % TAB_WIDTH) for width in column_max_width ]
+# extend 2da entries to match width
+formatted2da = [ [ row[col].ljust(column_width[col]) for col, value in enumerate(row) ] for row in raw2da ]
+# print
+for row in formatted2da:
+    print(" ".join(row).rstrip())
